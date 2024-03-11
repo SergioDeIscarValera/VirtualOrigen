ebda300646b6c85cf006e57eb65595eb78f66f68
SergioDeIscarValera <seroigres888@gmail.com>
2024-03-11 14:03:27 +0100

    Init 0.0.1

---
+++ VirtualOrigenServer/VirtualOrigenServer/VirtualOrigenServer/Program.cs
@@ -0,0 +1,23 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Firestore;
+using Google.Cloud.Firestore.V1;
+
+Console.WriteLine("Init server...");
+
+var credentials = GoogleCredential.FromFile("../../../ApiKey/firebase_credentials.json");
+
+FirestoreDb db = FirestoreDb.Create("virtual-origen-tfg", new FirestoreClientBuilder
+{
+    Credential = credentials
+}.Build());
+
+CollectionReference collection = db.Collection("ejemplo");
+DocumentReference document = collection.Document("ejemplo1");
+
+DocumentSnapshot snapshot = await document.GetSnapshotAsync();
+
+if (snapshot.Exists)
+{
+    var name = snapshot.GetValue<string>("nombre");
+    Console.WriteLine(name);
+}
